e253b34147f0a2788b0dad190bcda650db8175b9
Tedder <25165500+TheTedder@users.noreply.github.com>
2020-01-22 17:56:37 -0500

    prevent Aferbirth stages from loading

--- main.lua
+++ main.lua
@@ -1,2 +1,17 @@
 local mod = RegisterMod("The Binding of Isaac Classic", 1)
+local game = Game()
 
+function mod:PostNewLevel()
+    local level = game:GetLevel()
+    local stage = level:GetStage()
+    local stagetype = level:GetStageType()
+    if stagetype == StageType.STAGETYPE_AFTERBIRTH then
+        print("replacing stage")
+        -- TODO: use the seed to calculate this
+        level:SetStage(stage, StageType.STAGETYPE_ORIGINAL)
+        -- This is a hack. If you don't do this then the room graphics don't update.
+        game:ChangeRoom(level:GetStartingRoomIndex())
+    end
+end
+
+mod:AddCallback(ModCallbacks.MC_POST_NEW_LEVEL, mod.PostNewLevel)
